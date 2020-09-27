1aac320e13304d2a10cd072bedc329b72d930858
Thomas Brannan <th.c.brannan@gmail.com>
2020-09-27 12:11:13 -0700

    made rmap() use a ternary operator, just because I wanted to practice those

--- src/recursion.js
+++ src/recursion.js
@@ -391,10 +391,12 @@ var countOccurrence = function(array, value) {
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
-  if (array.length === 1) {
-    return [callback(array[0])];
-  }
-  return [callback(array[0])].concat(rMap(array.slice(1), callback));
+  // if (array.length === 1) {
+  //   return [callback(array[0])];
+  // }
+  // return [callback(array[0])].concat(rMap(array.slice(1), callback));
+  return array.length === 1 ? [callback(array[0])]
+                            : [callback(array[0])].concat(rMap(array.slice(1), callback));
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
