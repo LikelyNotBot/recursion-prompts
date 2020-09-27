46b07a164099dedeff802337dcf4dc6050dd0b90
Thomas Brannan <th.c.brannan@gmail.com>
2020-09-27 11:53:18 -0700

    passed tests for countOccurrence()

--- src/recursion.js
+++ src/recursion.js
@@ -378,6 +378,14 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) {
+    return 0;
+  }
+  var increment = 0;
+  if (array[0] === value) {
+    increment = 1;
+  }
+  return (increment + countOccurrence(array.slice(1), value));
 };
 
 // 21. Write a recursive version of map.
